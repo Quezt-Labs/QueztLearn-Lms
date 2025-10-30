fb763518723a0e008ad7c77fc85a2072755b5835
imshiv4m <shivamjha190@gmail.com>
2025-10-30 22:31:50 +0530

    refactor: Adjust sidebar width for improved layout consistency and user experience, changing dimensions from 300px to 220px.

--- src/components/ui/sidebar.tsx
+++ src/components/ui/sidebar.tsx
@@ -87,11 +87,11 @@ export const DesktopSidebar = ({
     <>
       <motion.div
         className={cn(
-          "h-full px-2 py-4 hidden md:flex md:flex-col bg-neutral-100 dark:bg-neutral-800 w-[300px] shrink-0",
+          "h-full px-2 py-4 hidden md:flex md:flex-col bg-neutral-100 dark:bg-neutral-800 w-[220px] shrink-0",
           className
         )}
         animate={{
-          width: animate ? (open ? "300px" : "60px") : "300px",
+          width: animate ? (open ? "220px" : "60px") : "220px",
         }}
         onMouseEnter={() => setOpen(true)}
         onMouseLeave={() => setOpen(false)}
